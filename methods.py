3ca99c8c1da1da85d242422e3285a31e65a8da14
RivianSoldier <bilzims.sg@gmail.com>
2023-05-04 09:12:47 -0300

    implementação dos metodos e seus testes

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum_values(num1, num2):
+    res = num1 + num2
+    return res
+
+def sub(num1, num2):
+    res = num1 - num2
+    return res
+
+def mult(num1, num2):
+    res = num1 * num2
+    return res
+
+def div(num1, num2):
+    res = num1 / num2
+    return res
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum_values():
+    # given number 1 = 5 and number 2 = 5
+    num1 = 5
+    num2 = 5
+
+    # when we calculate the sum
+    output = methods.sum_values(num1, num2)
+
+    # then the result must be 10
+    assert output == 10
+
+def test_subtraction():
+    # given number 1 = 10 and number 2 = 5
+    num1 = 10
+    num2 = 5
+
+    # when we calculate the subtraction of the values
+    output = methods.sub(num1, num2)
+
+    # then the result must be 5
+    assert output == 5
+
+def test_multiplication():
+    # given number 1 = 5 and number 2 = 5
+    num1 = 5
+    num2 = 5
+
+    # when we calculate the multiplication of the values
+    output = methods.mult(num1, num2)
+
+    # then the result must be 25
+    assert output == 25
+
+def test_division():
+    # given number 1 = 10 and number 2 = 5
+    num1 = 10
+    num2 = 5
+
+    # when we calculate the division of the values
+    output = methods.div(num1, num2)
+
+    # then the result must be 2
+    assert output == 2
